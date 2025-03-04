a28826fccd2b4835ae4988eccccc1a44d9b37769
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2025-03-04 13:25:30 +0200

    UI - Setup color mode for Bootstrap - Color mode service - listen to changes on app.component.ts

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 import {RouterOutlet} from "@angular/router";
+import {ColorModeService} from "./services/color-mode/color-mode.service";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,14 @@ import {RouterOutlet} from "@angular/router";
   imports: [RouterOutlet],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private colorModeService: ColorModeService,
+    private renderer: Renderer2,
+  ) {
+    this.colorModeService.theme$.subscribe(theme => {
+      // Update the `data-bs-theme` attribute on the HTML element
+      this.renderer.setAttribute(document.documentElement, 'data-bs-theme', theme)
+    })
+
+  }
 }
