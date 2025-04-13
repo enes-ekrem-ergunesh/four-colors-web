4f7ffc75fbafb8eea24f4f8f7aafefb23088c069
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2025-04-13 11:58:41 +0200

    API: rename special log file and ignore it

--- api/special_logger.py
+++ api/special_logger.py
@@ -2,7 +2,7 @@ import datetime
 
 def special_log(route: str, description: str, user_id: int = None, token_id: int = None, email: str = None):
     current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    with open('special-log.txt', 'a') as log_file:
+    with open('special.log', 'a') as log_file:
         if user_id and token_id:
             log_file.write(f"{current_time} | {route} | {user_id} | {token_id} | {description}\n")
         elif email:
